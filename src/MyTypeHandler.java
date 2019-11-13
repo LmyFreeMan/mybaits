53670dca9e2a71f49cbf65ba4d4798abab28b0bb
Coder <2515946609@qq.com>
2019-11-13 21:38:41 +0800

    类型处理器（typeHandlers）

---
+++ src/MyTypeHandler.java
@@ -0,0 +1,26 @@
+import org.apache.ibatis.type.BaseTypeHandler;
+import org.apache.ibatis.type.JdbcType;
+import org.apache.ibatis.type.MappedJdbcTypes;
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+@MappedJdbcTypes(JdbcType.VARCHAR)
+public class MyTypeHandler extends BaseTypeHandler<String> {
+    @Override
+    public void setNonNullParameter(PreparedStatement preparedStatement, int i, String s, JdbcType jdbcType) throws SQLException {
+        preparedStatement.setString(i,s);
+    }
+    @Override
+    public String getNullableResult(ResultSet resultSet, String s) throws SQLException {
+        return null;
+    }
+    @Override
+    public String getNullableResult(ResultSet resultSet, int i) throws SQLException {
+        return null;
+    }
+    @Override
+    public String getNullableResult(CallableStatement callableStatement, int i) throws SQLException {
+        return null;
+    }
+}
