0145a7556e42b4b36799db501354a5608dba79a7
Coder <2515946609@qq.com>
2019-11-14 20:37:00 +0800

    数据模型接口

--- src/dao/EmployeeDao.java
+++ src/dao/EmployeeDao.java
@@ -1,5 +1,15 @@
 package dao;
+import org.apache.ibatis.annotations.MapKey;
 import pojo.Employee;
+import java.util.List;
+import java.util.Map;
 public interface EmployeeDao {
     public Employee selectEmpById(int id);
+    public void deleteEmp(int id);
+    public void updateEmp(Employee emp);
+    public void insertEmp(Employee employee);
+    public Employee selectEmpByIdAndLastName(int id,String last_name);
+    public List<Employee> selectAllEmp();
+    @MapKey("id")
+    public Map<String,Employee> selectAllEmp1();
 }
