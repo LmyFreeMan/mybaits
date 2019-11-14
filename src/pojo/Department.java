ee0410b98a0fb8f3df5ca7a95cfbf66ed26e1e29
Coder <2515946609@qq.com>
2019-11-14 20:43:01 +0800

    javabean

---
+++ src/pojo/Department.java
@@ -0,0 +1,31 @@
+package pojo;
+
+public class Department {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+}
